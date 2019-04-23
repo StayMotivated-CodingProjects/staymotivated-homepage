ba30b178dc39ced03ecafc1600891383c0263406
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-04-23 22:57:01 +0200

    first commit - project start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import './Grid.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="container">
+
+        <header>
+
+          <h1>Stay Motivated - Coding Projects</h1>
+
         </header>
+
+        <menu>
+
+        </menu>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
       </div>
     );
   }
